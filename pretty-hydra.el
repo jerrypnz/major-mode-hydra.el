d8d2e72fab68d16b959fcfa8f6008d14b8840ede
Jerry Peng <pr2jerry@gmail.com>
2018-03-12 22:50:26 +1300

    add file header to pretty-hydra.el

--- pretty-hydra.el
+++ pretty-hydra.el
@@ -3,6 +3,27 @@
 ;; Copyright (C) 2018 Jerry Peng
 
 ;; Author: Jerry Peng <pr2jerry@gmail.com>
+;; URL: https://github.com/jerrypnz/major-mode-hydra.el
+;; Keywords: hydra
+;; Version: 0.1.0
+;; Package-Requires: ((hydra "0.13.4") (s "1.10.0") (dash "2.12.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
